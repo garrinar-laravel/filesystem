5ab1434e4b9a46325ef05c88b648c89665cc4dcf
garrinar <garrinar@gmail.com>
2016-05-05 19:31:14 +0300

    composer init

---
+++ Filesystem/ServiceProvider/DistributedFilesystemServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Garrinar
+ * Date: 05.05.2016
+ * Time: 19:28
+ */
+
+namespace Garrinar\Filesystem\ServiceProvider;
+
+use Garrinar\Filesystem\Adapter\DistributedFilesystemAdapter;
+use Storage;
+use League\Flysystem\Filesystem;
+use Illuminate\Support\ServiceProvider;
+
+class DistributedFilesystemServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Storage::extend('distributed', function($app, $config) {
+            return new Filesystem(new DistributedFilesystemAdapter());
+        });
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
