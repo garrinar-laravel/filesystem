6f1c98afe04ddebc14a5d65d24e7522f19c3aab1
garrinar <garrinar@gmail.com>
2016-05-06 13:22:06 +0300

    composer init

---
+++ src/Distributed/Filesystem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Garrinar\Filesystem\Distributed;
+
+use Symfony\Component\Finder\Finder;
+use League\Flysystem\Filesystem as FlySystem;
+
+class Filesystem extends FlySystem
+{
+
+}
--- src/Distributed/Manager.php
+++ src/Distributed/Manager.php
@@ -5,6 +5,8 @@ namespace Garrinar\Filesystem\Distributed;
 use Illuminate\Filesystem\FilesystemManager;
 use Illuminate\Support\Arr;
 use Garrinar\Filesystem\Distributed\Adapter as DistributedAdapter;
+use League\Flysystem\AdapterInterface;
+
 
 
 class Manager extends FilesystemManager
@@ -18,8 +20,22 @@ class Manager extends FilesystemManager
             ? DistributedAdapter::SKIP_LINKS
             : DistributedAdapter::DISALLOW_LINKS;
 
-        return $this->adapt($this->createFlysystem(new DistributedAdapter(
+        return $this->adapt($this->createDistribitedFilesystem(new DistributedAdapter(
             $config['root'], LOCK_EX, $links, $permissions
         ), $config));
+    }
+
+    /**
+     * Create a Flysystem instance with the given adapter.
+     *
+     * @param  \League\Flysystem\AdapterInterface  $adapter
+     * @param  array  $config
+     * @return \League\Flysystem\FlysystemInterface
+     */
+    protected function createDistributedFilesystem(AdapterInterface $adapter, array $config)
+    {
+        $config = Arr::only($config, ['visibility']);
+
+        return new Filesystem($adapter, count($config) > 0 ? $config : null);
     }
 }
