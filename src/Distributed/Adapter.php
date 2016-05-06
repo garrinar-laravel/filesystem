92528df3b8f5151716733bddb63373b91397aa37
garrinar <garrinar@gmail.com>
2016-05-06 15:38:58 +0300

    composer init

--- src/Distributed/Adapter.php
+++ src/Distributed/Adapter.php
@@ -10,7 +10,37 @@ namespace Garrinar\Filesystem\Distributed;
 
 use League\Flysystem\Adapter\Local;
 
-class Adapter extends Local 
+class Adapter extends Local
 {
+    public function applyPathPrefix($path)
+    {
+        $path = $this->applyDistributedPathPrefix($path);
+        return $path;
+    }
+
+    public function applyDistributedPathPrefix($path)
+    {
+        return parent::applyPathPrefix($this->getDistributedPrefix($path).$path);
+    }
+
+    public function getDistributedPrefix($path)
+    {
+        $md5FileName = md5($path);
+        $prefixes = collect([
+            mb_substr($md5FileName, 0, 3),
+            mb_substr($md5FileName, 3, 3),
+            mb_substr($md5FileName, 6, 3),
+        ]);
+        $dirPath = '';
+        $prefixes->each(function($prefix, $key) use (&$dirPath) {
+            $dirPath .= $this->pathSeparator.$prefix;
+            $fullPath = parent::getPathPrefix().$dirPath;
+            if(!is_dir($fullPath)) {
+                @mkdir(parent::getPathPrefix() . $dirPath);
+            }
+        });
+
+        return $dirPath.$this->pathSeparator;
+    }
 
 }
